451883308b4c077ac225194eaa86e637648b3a11
Iggy-V <ignasvolcokas@gmail.com>
2024-03-24 10:13:49 -0400

    bugs

--- llmAPI.py
+++ llmAPI.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 def get_suggestion(query):
     client = OpenAI(
         base_url="https://api.endpoints.anyscale.com/v1",
-        api_key="esecret_tq3mcn3tpxb3vi5nbk6away7d4"  # Replace with your actual OpenAI API key
+        api_key="YOUR_API_KEY"  # Replace with your actual OpenAI API key
     )
     # Note: not all arguments are currently supported and will be ignored by the backend.
     chat_completion = client.chat.completions.create(
